7249d20e1473390694b67d98c3830a3e08a40389
jorisvm <jorisvanmontfort@gmail.com>
2024-04-05 00:04:07 +0200

    Small fix to post ID in ajax call.

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     if (false == document.body.classList.contains("archive")) {
-        // Get price info with ajax
         let url = wp_post_views_ajax_object.ajaxurl; // this is the object instantiated in wp_localize_script function);
 
         let data = new FormData();
         data.append('action', 'wppv_counter');
-        data.append('post_id', 'custom_art');
+        data.append('post_id', p_post_views_ajax_object.post_id);
         data.append('nonce', wp_post_views_ajax_object.nonce);
         
         let xhttp = new XMLHttpRequest();
