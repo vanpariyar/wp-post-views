fd48518f705a55363a2bc77a454d9ca1e58453d1
Ronak J Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2024-04-05 15:57:58 +0530

    Update ajax.js

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    if (false == document.body.classList.contains("archive")) {
+    if ( true == document.body.classList.contains("archive") || true == document.body.classList.contains("home") || true == document.body.classList.contains("blog") ) {
+        return;
+    } else {
         let url = wp_post_views_ajax_object.ajaxurl;
 
         let data = new FormData();
         data.append('action', 'wppv_counter');
         data.append('post_id', wp_post_views_ajax_object.post_id);
         data.append('nonce', wp_post_views_ajax_object.nonce);
-        
         let xhttp = new XMLHttpRequest();
-        xhttp.onload = function() {
-            let res = JSON.parse(this.responseText);
-        }
 
         xhttp.open("POST", url);
         xhttp.send(data);
