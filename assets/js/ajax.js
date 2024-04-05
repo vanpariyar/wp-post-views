01d155ec86771cea20721458cdceaddc91eb957c
jorisvm <jorisvanmontfort@gmail.com>
2024-04-05 11:32:51 +0200

    No comments in JS code.

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     if (false == document.body.classList.contains("archive")) {
-        let url = wp_post_views_ajax_object.ajaxurl; // this is the object instantiated in wp_localize_script function);
+        let url = wp_post_views_ajax_object.ajaxurl;
 
         let data = new FormData();
         data.append('action', 'wppv_counter');
@@ -10,8 +10,6 @@ document.addEventListener('DOMContentLoaded', function () {
         let xhttp = new XMLHttpRequest();
         xhttp.onload = function() {
             let res = JSON.parse(this.responseText);
-            //Do something with the result from server
-            // console.log(res);
         }
 
         xhttp.open("POST", url);
